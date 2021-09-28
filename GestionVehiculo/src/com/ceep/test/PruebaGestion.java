05014352902c7ee921296a7a982bc505283a1f1a
Marcosss2 <marcosparra20@hotmail.com>
2021-09-28 12:29:47 +0200

    Initial commit

---
+++ GestionVehiculo/src/com/ceep/test/PruebaGestion.java
@@ -0,0 +1,15 @@
+package com.ceep.test;
+
+import com.ceep.domain.Vehiculo;
+
+public class PruebaGestion {
+
+    public static void main(String[] args) {
+
+        Vehiculo C1 = new Vehiculo("HHH-0000", "Citroen", "C5", "Blanco", 20000.500, true);
+        
+        System.out.println(C1);
+
+    }
+
+}
